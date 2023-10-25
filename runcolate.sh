a8b8b726f56d0b2cf10d41cadceceb8ac378593b
santiago1234 <santiagogmm@hotmail.com>
2023-10-25 15:07:21 -0500

    run colate

---
+++ runcolate.sh
@@ -0,0 +1,1 @@
+/data/users/smedina/software/Colate-devel/binaries/v0.1.4_x86_64_static/bin/Colate --mode local_ancestry -i data/example -o output --chr chr.txt --bins 2,7,0.2 --num_bootstraps 100 --poplabels data/assignment.txt
